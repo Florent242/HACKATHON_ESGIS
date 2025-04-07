c643dcabc8adc63b0a04f7d4c9f3406437d83d73
Florent BOUDZOUMOU <109081275+Florent242@users.noreply.github.com>
2025-04-07 21:05:25 +0000

    suppressions des bdd et tables portant a confusions

--- database/seed.sql
+++
@@ -1,5 +0,0 @@
-USE hackathon_db;
-
--- Création d'un utilisateur organisateur (mot de passe: organisateur123)
-INSERT INTO users (username, email, password, role, full_name) VALUES 
-('organisateur', 'organisateur@esgis.bj', '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', 'organisateur', 'Organisateur Test');
